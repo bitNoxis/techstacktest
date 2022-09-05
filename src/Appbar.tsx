aaba9c40d3629652b73c53229ba52a7a1b5e5ba4
Finn <finn.k2012@gmail.com>
2022-09-05 15:05:24 +0200

    Funktionierender Stand

--- src/Appbar.tsx
+++ src/Appbar.tsx
@@ -8,7 +8,6 @@ import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import NewFoodButton from "./FoodButton";
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
