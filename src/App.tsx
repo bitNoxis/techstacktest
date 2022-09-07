ae8d96eda9f78cbdbf30d19ef51c7adf961f5643
Finn <finn.k2012@gmail.com>
2022-09-07 16:10:33 +0200

    Funktionierender Stand

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import SearchAppBar from "./Appbar";
 import FoodAnzeige from "./FoodAnzeige";
-import NewFood from "./NewFood";
+import Food from "./Food";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar/>
-        <NewFood/>
+        <Food/>
     </div>
   );
 }
---
+++ src/Food.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import PickerAblaufdatum from "./PickerAblaufdatum";
+import {Container} from "@mui/material";
+import Paper from "@mui/material/Paper";
+import {useEffect, useState} from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Button from "@mui/material/Button";
+import {DatePicker} from "@mui/x-date-pickers/DatePicker";
+
+export default function NewFood() {
+    const paperStyle = {padding:'50px 20px',width:600, margin: "20px auto"}
+    const date = new Date();
+    const actualDate = date.getDate();
+    date.setDate(actualDate);
+    const defaultValue = date.toLocaleDateString('en-DE');
+    const refreshPage = ()=>{
+        window.location.reload();
+    }
+
+
+    const [productname, setProductName]= useState(String);
+    const [expirationdate, setExpirationDate] = useState(defaultValue);
+    const food={productname, expirationdate}
+    const [foods, setFoods] = useState([]);
+
+    const handleClick=(e: { preventDefault: () => void; })=>{
+        e.preventDefault()
+
+        console.log(food)
+        fetch("http://localhost:8080/food/add", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(food)
+        }).then(()=> {
+            console.log("New Food added")
+        })
+    }
+
+    const handleDateChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        console.log(event.target.value);
+        setExpirationDate(event.target.value);
+    };
+
+    useEffect(()=> {
+        fetch("http://localhost:8080/food/getAll")
+            .then(res => res.json())
+            .then((result) => {
+                setFoods(result);
+            })
+    },[])
+
+    return (
+        <Container>
+            <Paper elevation={3} style={paperStyle}>
+                <form>
+                <h1>Produkt hinzufügen</h1>
+                <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)': {m: 1, width: '20ch'},
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                <TextField id="productname" label="Produktname" variant="outlined"
+                           value={productname}
+                           onChange={e => setProductName(e.target.value)}/>
+                <TextField
+                    id="date"
+                    label="Ablaufdatum"
+                    type="date"
+                    value={expirationdate}
+                    onChange={handleDateChange}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                </Box>
+                <Button variant="contained" endIcon={<AddCircleIcon/>} onClick={handleClick}>
+                    Hinzufügen
+                </Button>
+                </form>
+                <h1>Übersicht</h1>
+                <button onClick={refreshPage}>Refresh</button>
+                    {foods.map(food=>(
+                        <Paper elevation={6} style={{margin:"10px",padding: "15px", textAlign:"left"}} key={food['productid']}>
+                            Id:{food['productid']}<br/>
+                            Produkt:{food['productname']}<br/>
+                            Ablaufdatum:{food['expirationdate']}
+
+                        </Paper>
+                    ))
+                    }
+
+
+            </Paper>
+        </Container>
+    );
+}
